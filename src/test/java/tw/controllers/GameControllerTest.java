f9f4ceff607e9e89864ddc3e4ee32f053ac43410
nn <npr_0815@163.com>
2019-05-23 19:02:12 +0800

    update GameControllerTest.java

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -3,12 +3,14 @@ package tw.controllers;
 import org.junit.Before;
 import org.junit.Test;
 import tw.commands.GuessInputCommand;
+import tw.core.Answer;
+import tw.core.Game;
+import tw.core.model.GuessResult;
 import tw.views.GameView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 /**
@@ -28,16 +30,35 @@ public class GameControllerTest {
 
     @Test
     public void should_show_begin_information() throws Exception{
-        GameView gameView = new GameView();
-        gameView.showBegin();
-        assertThat(systemOut()).isEqualTo("------Guess Number Game, You have 6 chances to guess!  ------\r\n");
+        Game game = mock(Game.class);
+        GameView gameView = mock(GameView.class);
+        GameController gameController = new GameController(game, gameView);
+
+        gameController.beginGame();
+
+        verify(gameView).showBegin();
     }
 
     @Test
     public void shoule_execute_play_method()throws Exception{
-        GameController gameController = mock(GameController.class);
-        GuessInputCommand guessInputCommand = mock(GuessInputCommand.class);
-        gameController.play(guessInputCommand);
-            verify(gameController,times(1)).play(guessInputCommand);
+        GuessInputCommand command = mock(GuessInputCommand.class);
+        Game game = mock(Game.class);
+        GameView gameView = mock(GameView.class);
+        Answer answer = new Answer();
+        GuessResult guessResult = new GuessResult("",answer);
+
+        when(game.checkCoutinue()).thenReturn(true).thenReturn(false);
+        when(command.input()).thenReturn(answer);
+        when(game.guess(answer)).thenReturn(guessResult);
+
+        GameController gameController = new GameController(game, gameView);
+        gameController.play(command);
+
+        verify(game, times(2)).checkCoutinue();
+        verify(game).guess(answer);
+        verify(command).input();
+        verify(gameView).showGuessResult(guessResult);
+        verify(gameView).showGuessHistory(game.guessHistory());
+        verify(gameView).showGameStatus(game.checkStatus());
     }
 }
